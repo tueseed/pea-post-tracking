77eae06c87798e3d57b41fd077c3194272982069
tueseed <tue_seed@hotmail.com>
2019-10-02 20:21:45 +0700

    sdadsad

--- gettoken.php
+++ gettoken.php
@@ -1,6 +1,5 @@
 <?php
     $curl = curl_init();
-
     curl_setopt_array($curl, array(
       CURLOPT_URL => "https://trackwebhook.thailandpost.co.th/post/api/v1/authenticate/token",
       CURLOPT_RETURNTRANSFER => true,
@@ -11,9 +10,7 @@
       CURLOPT_CUSTOMREQUEST => "POST",
       CURLOPT_HTTPHEADER => array(
         "Authorization: Token O:NUEtWeHzQ$Y7H-H/TLQvO3KMLzYpYPTKOtLJaZJT:N+L^HTA6A7S3V:HASGxOdXZW|CnCjK:L0HqWACWQKINW:LeV/WlCsK8",
-        "Content-Type: application/json",
-        "Postman-Token: 4942afaf-c40b-4075-b80d-8e02725aa0a9",
-        "cache-control: no-cache"
+        "Content-Type: application/json"
       ),
     ));
     
