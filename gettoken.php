af87dc77ce2ff7304308ab29715daa6a2bbd2261
tueseed <tue_seed@hotmail.com>
2019-10-02 20:26:45 +0700

    asdasdas

--- gettoken.php
+++ gettoken.php
@@ -1,27 +1,21 @@
 <?php
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://trackwebhook.thailandpost.co.th/post/api/v1/authenticate/token",
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "POST",
-      CURLOPT_HTTPHEADER => array(
-        "Authorization: Token OeB@M?PhFkFVPnG8M4T;L^HMQEG=RWX^MLMaQNRZXrFzW3KjN@Y$VCOoAsOYOSB6PqJ:C~E$W|J:PzDoW;K9E@ZL4FXG6VUFRK:",
-        "Content-Type: application/json"
-      ),
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
+    $token = 'OeB@M?PhFkFVPnG8M4T;L^HMQEG=RWX^MLMaQNRZXrFzW3KjN@Y$VCOoAsOYOSB6PqJ:C~E$W|J:PzDoW;K9E@ZL4FXG6VUFRK:';
+    $url = 'https://trackwebhook.thailandpost.co.th/post/api/v1/authenticate/token';
+    $headers = [
+        'Authorization: Token '. $token,
+        'Content-Type: application/json'
+    ];
+     
+    $ch = curl_init();
+    curl_setopt( $ch, CURLOPT_URL, $url );
+    // curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($content) );
+    curl_setopt( $ch, CURLOPT_POST, true );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ 
+    $result = curl_exec( $ch );
+    curl_close($ch);
+       
+    return json_decode($result, true);
 ?>
